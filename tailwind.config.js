8c98bf5375845424bcea6fb6ebaaeed78ec95bd3
nickzmoro <nicolasmororuiz@gmail.com>
2025-03-22 13:22:18 -0300

    chore: font and images & curriculum added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "radial-gradient":
+          "radial-gradient(circle at center, #ff0000 0%, #0000ff 100%)",
+      },
+    },
   },
   plugins: [],
 };
