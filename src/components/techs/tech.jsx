f687746965b163f3f44cbd89a83e697523858c04
nickzmoro <nicolasmororuiz@gmail.com>
2025-04-10 21:04:38 -0300

    feat: component tech for projects

---
+++ src/components/techs/tech.jsx
@@ -0,0 +1,18 @@
+const Tech = ({ img, name }) => {
+  return (
+    <div className="group flex gap-1 items-center px-[10px] py-[2px] bg-[#ffffff0e] rounded-full border border-[#ffffff17] cursor-default w-fit">
+      <img
+        src={img}
+        alt={name}
+        className={`
+            ${name === "JavaScript" ? "rounded-[2px] py-[3px]" : "w-[20px]"}
+            h-[20px] saturate-0 group-hover:saturate-100 transition-all ease-in-out duration-200`}
+      />
+      <p className="text-[#808080] text-[0.95rem] font-[500] group-hover:text-[#ffffffc4] transition-all ease-in-out duration-200 cursor-default">
+        {name}
+      </p>
+    </div>
+  );
+};
+
+export default Tech;
