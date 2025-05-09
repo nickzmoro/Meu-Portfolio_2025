cc0b2f5179123740d97acbcf7663bab4edbfa94c
nickzmoro <nicolasmororuiz@gmail.com>
2025-05-08 21:47:41 -0300

    chore: keyframes in "tailwind.config.js" added

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,21 @@ export default {
         "linear-gradient":
           "linear-gradient(to bottom, #141841 0%, #0C0E23 100%)",
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        slideUp: {
+          "0%": { transform: "translateY(20px)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.5s ease-in",
+        slideUp: "slideUp 0.5s ease-out 0.3s forwards",
+        zoom: "zoom 1.5s cubic-bezier(0.19, 1, 0.22, 1) forwards",
+      },
     },
   },
   plugins: [],
